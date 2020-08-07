4ac98d7a91aebff98fedbbcebb30bd07e1ed7c87
perinazzoo <gabrielperinazo@gmail.com>
2020-08-06 23:43:51 -0300

    🔖 add v3.0 with api call

--- app/src/main/java/com/gabrielperinazzo/exercicio3/MainActivity.java
+++ app/src/main/java/com/gabrielperinazzo/exercicio3/MainActivity.java
@@ -2,68 +2,119 @@ package com.gabrielperinazzo.exercicio3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import com.google.gson.*;
+import android.util.JsonReader;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DecimalFormat;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView currentDollar, currentEuro;
     EditText currentRealInput;
-    TextView currentDollarValue, currentEuroValue;
     Button convertButton, clearButton, devByButton;
-
+    final String[] currentEuroValue = {"..."};
+    final String[] currentDollarValue = {"..."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText currentRealInput;
-
+//
         final EditText realValue = findViewById(R.id.currentRealInput);
-
+//
+        final TextView currentDollarView = findViewById(R.id.currentDollar);
+        final TextView currentEuroView = findViewById(R.id.currentEuro);
         Button btnConvert = findViewById(R.id.convertButton);
         Button btnClear = findViewById(R.id.clearButton);
         Button btnDev = findViewById(R.id.devByButton);
-
+//
         btnConvert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 reload();
-                return;
             }
         });
-        
+//
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 realValue.setText("");
                 reload();
-                return;
             }
         });
-
+//
         btnDev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Gabriel Perinazzo", Toast.LENGTH_SHORT).show();
-                return;
+            }
+        });
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL currencyEndpoint = new URL("https://economia.awesomeapi.com.br/json/all/USD-BRL,EUR-BRL");
+
+
+                    HttpsURLConnection myConnection = (HttpsURLConnection) currencyEndpoint.openConnection();
+
+                    if (myConnection.getResponseCode() == 200) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(myConnection.getInputStream()));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line+"\n");
+                        }
+                        br.close();
+
+                        String json = sb.toString();
+
+                        JsonObject convertedObject = new Gson().fromJson(json, JsonObject.class);
+                        currentDollarValue[0] = convertedObject.getAsJsonObject("USD").get("high").getAsString();
+                        currentEuroValue[0] = convertedObject.getAsJsonObject("EUR").get("high").getAsString();
+                        myConnection.disconnect();
+
+                        currentDollarView.setText("R$ " + currentDollarValue[0]);
+                        currentEuroView.setText("R$ " + currentEuroValue[0]);
+                    }
+                } catch (IOException e) {
+                    System.out.println("deu erro");
+                }
             }
         });
     }
 
     public void reload() {
         EditText currentRealInput;
-        TextView currentDollarValue, currentEuroValue;
+        TextView dollarFinal, euroFinal;
         Button convertButton, clearButton;
 
         final EditText realValue = findViewById(R.id.currentRealInput);
 
-        final TextView dollarValue = findViewById(R.id.currentDollarValue);
-        final TextView euroValue = findViewById(R.id.currentEuroValue);
+        final TextView dollarValue = findViewById(R.id.dollarFinal);
+        final TextView euroValue = findViewById(R.id.euroFinal);
 
 
         String stringValue = realValue.getText().toString();
@@ -76,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
 
         Double Value = Double.valueOf(stringValue);
 
-        dollarValue.setText(String.format("%.2f", Value / 4));
-        euroValue.setText(String.format("%.2f", Value / 5));
+        dollarValue.setText("R$ " + String.format("%.2f", Value / Double.parseDouble(currentDollarValue[0])));
+        euroValue.setText("R$ " + String.format("%.2f", Value / Double.parseDouble(currentEuroValue[0])));
     }
 }
